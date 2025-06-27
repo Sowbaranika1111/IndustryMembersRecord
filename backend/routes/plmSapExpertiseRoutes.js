20a65966cf846d2b1d072ed31fe11a44e67c3322
Manojguggilla <manojguggilla6@gmail.com>
2025-06-27 17:08:29 +0530

    Added plm-sap expertise

---
+++ backend/routes/plmSapExpertiseRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const {
+  
+  getAllExpertise,
+  deleteAllExpertise,
+  insertAllExpertise
+} = require("../controllers/dropDownValuesController/plmSapExpertiseController");
+
+router.get("/",getAllExpertise);
+
+router.delete("/delete-all", deleteAllExpertise);
+
+router.post("/insert-many", insertAllExpertise);
+
+module.exports = router;
